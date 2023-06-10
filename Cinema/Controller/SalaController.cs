9398db55abbe1d1d53c15b2446d3ef8fe9df59a3
Eduardo Ramalho <2220854@my.ipleiria.pt>
2023-06-10 18:22:39 +0100

    FUNCIONARIOS

--- Cinema/Controller/SalaController.cs
+++ Cinema/Controller/SalaController.cs
@@ -30,8 +30,8 @@ namespace Cinema.Controller
                     int valor = 1;
                     int totalLugares= coluna1 * fila1;
 
+                    var cl = new Sala { nome = nome1, lugar = arraySala, numeroDeLugares = totalLugares };
 
-                    
                     for (int i = 0; i < arraySala.GetLength(0); i++)
                     {
                         for (int j = 0; j < arraySala.GetLength(1); j++)
@@ -40,7 +40,6 @@ namespace Cinema.Controller
                             valor++;
                         }
                     }
-                    var cl = new Sala { nome = nome1, lugar = arraySala, numeroDeLugares = totalLugares };
 
                     db.Salas.Add(cl);
                     db.SaveChanges();
